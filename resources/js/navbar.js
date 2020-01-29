6d40ba05b772adb463bb45a264026075de5fb219
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-01-29 14:25:42 +0100

    Mobile language switch toggling

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -2,13 +2,41 @@ $(document).ready(function() {
   // Hamburger menu animation
   var menuBtn = $('#mobileMenuBtn');
   var menu = $('#mainMenu');
+  var langSwitch = $('#languageSwitch');
+  var langBtn = $('#languageTogglerBtn');
+
+
   menuBtn.click(function(){
+
+    if (langBtn.hasClass('is-active')) {
+      langBtn.removeClass('is-active');
+      langSwitch.css('left', '100vw');
+    } 
+
     if (menuBtn.hasClass('is-active')) {
       menuBtn.removeClass('is-active');
       menu.css('left', '100vw');
     } else {
       menuBtn.addClass('is-active');
       menu.css('left', '0');
+    }
+  });
+
+  // Mobile language switch
+
+  langBtn.click(function(){
+
+    if (menuBtn.hasClass('is-active')) {
+      menuBtn.removeClass('is-active');
+      menu.css('left', '100vw');
+    }
+
+    if (langBtn.hasClass('is-active')) {
+      langBtn.removeClass('is-active');
+      langSwitch.css('left', '100vw');
+    } else {
+      langBtn.addClass('is-active');
+      langSwitch.css('left', '0');
     }
   });
 });
