2b15b828e5bb66e755489575d3f57abbcc7e9347
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-01-29 10:25:30 +0100

    Mobile menu toggle and animation

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   // Hamburger menu animation
   var menuBtn = $('#mobileMenuBtn');
+  var menu = $('#mainMenu');
   menuBtn.click(function(){
     if (menuBtn.hasClass('is-active')) {
       menuBtn.removeClass('is-active');
+      menu.css('left', '100vw');
     } else {
       menuBtn.addClass('is-active');
+      menu.css('left', '0');
     }
   });
 });
