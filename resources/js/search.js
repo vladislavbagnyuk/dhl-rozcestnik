cbe6a66b080acf3ef286ea4b2b3cb14d2e878c04
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-02-01 11:22:37 +0100

    Search finished

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,19 +1,10 @@
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
+$(document).ready(function() {
+  $('.search input[type=text]').focus(function(){
+    $('.autocomplete').show();
+    $('.autocomplete').css('height', 'auto');
+  });
+  $('.search input[type=text]').blur(function(){
+    $('.autocomplete').css('height', 0);
+    $('.autocomplete').hide();
+  });
+});
