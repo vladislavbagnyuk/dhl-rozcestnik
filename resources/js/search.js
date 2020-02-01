8650bf59e179f585ebb29f96f3811a0d45da9609
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-02-01 14:32:02 +0100

    Fixed search bug

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
-  $('.search input[type=text]').focus(function(){
+
+  var input = $('.search input[type=text]');
+
+  input.click(function(){
     $('.autocomplete').show();
     $('.autocomplete').css('height', 'auto');
   });
-  $('.search input[type=text]').blur(function(){
-    $('.autocomplete').css('height', 0);
-    $('.autocomplete').hide();
-  });
+
+$('.autocomplete ul li').click(function(){
+  var text = $(this).contents().get(0).nodeValue;
+  input.val(text);
+  $('.autocomplete').css('height', 0);
+  $('.autocomplete').hide();
+});
+
+$(document).on("click", function(event){
+  if(!$(event.target).closest(".search input[type=text]").length){
+    if(!$(event.target).closest(".autocomplete").length){
+      // Showing the hint message
+      $('.autocomplete').css('height', 0);
+      $('.autocomplete').hide();
+    }
+  }
+});
+
 });
