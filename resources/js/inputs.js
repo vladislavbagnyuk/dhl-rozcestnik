ccf7d1385532b10018e2a51b6744242a8f6fff55
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-02-04 20:44:04 +0100

    Select JS

--- resources/js/inputs.js
+++ resources/js/inputs.js
@@ -1,24 +1,9 @@
 $(document).ready(function() {
 
-/*
-  $('input').each(function(){
-    var $element = $(this)
-
-    if ($element.val() == '') {
-      var $label = $("label[for='"+this.id+"']")
-      alert($label.text())
-    }
-
-  });
-*/
-
-
   $('input').focus(function(){
-    var $element = $(this);
     var $label = $("label[for='"+this.id+"']");
     $label.css('font-size', '11px');
   });
-
   $("input").blur(function(){
     var $element = $(this);
     if( !$element.val() ) {
@@ -26,6 +11,15 @@ $(document).ready(function() {
       $label.css('font-size', '16px');
     }
   });
-
+  $("select").change(function(){
+    var $label = $("label[for='"+this.id+"']");
+    if( !$(this).val() ) {
+      $label.css('top', '11px');
+      $label.css('font-size', '16px');
+    } else {
+      $label.css('top', '1px');
+      $label.css('font-size', '11px');
+    }
+  });
 
 });
