80c1b4f903c6f074fc4504e9b6695bc31c5bbe32
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-02-07 17:46:03 +0100

    Fixed search field bug in IE and Edge

--- resources/js/search.js
+++ resources/js/search.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
   var dropdown = $('.autocomplete');
   input.click(function(){
     dropdown.show();
+    input.addClass("search-focused");
     dropdown.css('height', 'auto');
   });
 
 $('.autocomplete ul li').click(function(){
   var text = $(this).contents().get(0).nodeValue;
   input.val(text);
+  input.removeClass("search-focused");
   dropdown.css('height', 0);
   dropdown.hide();
 });
@@ -16,7 +18,7 @@ $('.autocomplete ul li').click(function(){
 $(document).on("click", function(event){
   if(!$(event.target).closest(".search input[type=text]").length){
     if(!$(event.target).closest(".autocomplete").length){
-      // Showing the hint message
+      input.removeClass("search-focused");
       dropdown.css('height', 0);
       dropdown.hide();
     }
