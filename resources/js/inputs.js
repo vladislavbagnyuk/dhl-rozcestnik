8bef0690474e3354e109515f463f0db2dd772c44
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-02-24 17:35:05 +0100

    Fixed selects

--- resources/js/inputs.js
+++ resources/js/inputs.js
@@ -14,29 +14,45 @@ $(document).ready(function() {
   $("select").change(function(){
     var $label = $("label[for='"+this.id+"']");
     if( !$(this).val() ) {
-      $label.css('top', '11px');
+      //$label.css('top', '11px');
       $label.css('font-size', '16px');
     } else {
       $label.css('top', '1px');
       $label.css('font-size', '11px');
     }
   });
-  // Change colors of labels for disabled inputs
-  $('select:disabled').each(function() {
-    $this = $(this);
+
+
+  function enableSelect($this) {
+    $this.removeAttr("disabled");
+    $label = $('label[for="'+ $this.attr('id') +'"]');
+    if ($label.length > 0 ) {
+      $label.removeClass('disabledLabel');
+    }
+  }
+  function disableSelect($this) {
+    $this.attr('disabled', 'disabled');
     $label = $('label[for="'+ $this.attr('id') +'"]');
     if ($label.length > 0 ) {
       $label.addClass('disabledLabel');
+      $label.css('font-size', '16px');
     }
+  }
+  // Change colors of labels for disabled inputs
+  $('select:disabled').each(function() {
+    disableSelect($(this));
   });
   $('#form-new-1 select').change(function(){
     if( $(this).val() ) {
       $next = $(this).parent().next().children('select');
-      $next.removeAttr("disabled");
-      $label = $('label[for="'+ $next.attr('id') +'"]');
-      if ($label.length > 0 ) {
-        $label.removeClass('disabledLabel');
-      }
+      enableSelect($next);
+    } else {
+      $nextParent = $(this).parent().nextUntil("button");
+      $nextParent.each(function(){
+        $next = $(this).children("select");
+        $next.prop('selectedIndex',0);
+        disableSelect($next);
+      });
     }
   });
 });
