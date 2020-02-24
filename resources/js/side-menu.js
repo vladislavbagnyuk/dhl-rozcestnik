539adfd863f910f9dfeeac98a057dff972bfc7ab
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-02-24 21:03:07 +0100

    Only one submenu open

--- resources/js/side-menu.js
+++ resources/js/side-menu.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   $(".side-menu li span").click(function(){
     $this = $(this);
+    $(".side-menu li span").each(function(){
+      $span = $(this);
+      if ($span != $this) {
+        if ($span.hasClass("active")) {
+          $span.removeClass("active");
+          $span.children("img").attr("src", "resources/img/icons/arrow-down.svg");
+          $span.next("ul").animate({
+            height: 0
+          }, 300, $.bez([0.22, 0.61, 0.35, 1]) );
+        }
+      }
+    });
     $submenu = $this.next("ul");
     if ($submenu.height() == 0) {
       $this.addClass("active");
