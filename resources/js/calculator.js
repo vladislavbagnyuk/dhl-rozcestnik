70556010100fa6c6ebd40496a4c1f1e4ef051fae
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-03-01 23:39:47 +0100

    Changed calculator animation

--- resources/js/calculator.js
+++ resources/js/calculator.js
@@ -3,17 +3,7 @@ $(document).ready(function() {
 
   $('#calculator-form').submit(function(e){
     e.preventDefault();
-    $("#orderBtn").removeClass("disabled");
-    $("#phoneLink").removeClass("disabled");
-    $("#emailLink").removeClass("disabled");
-    $(".delimeter-container").removeClass("disabled");
-    $("#calcSubmitBtn").attr('disabled', 'disabled');
     $(".server-wait").show();
-    if ($(window).width() < 768) {
-      $('html, body').animate({
-        scrollTop: $("#result").offset().top - 70
-      }, 550, $.bez([0.22, 0.61, 0.35, 1]));
-    }
   });
 
   function validationSuccess() {
@@ -22,6 +12,15 @@ $(document).ready(function() {
     $("#calcSubmitBtn").css("background-color", "white");
     $("#calcSubmitBtn").css("color", "#D40511");
     $("#serverCircle").addClass("green");
+    $("#phoneLink").removeClass("disabled");
+    $("#emailLink").removeClass("disabled");
+    $(".delimeter-container").removeClass("disabled");
+    $("#calcSubmitBtn").attr('disabled', 'disabled');
+    if ($(window).width() < 768) {
+      $('html, body').animate({
+        scrollTop: $("#result").offset().top - 70
+      }, 550, $.bez([0.22, 0.61, 0.35, 1]));
+    }
   }
 
 });
