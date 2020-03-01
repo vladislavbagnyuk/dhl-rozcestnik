98212fabad21f0cb2cfbc9817642f453cae58a64
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-03-01 20:45:43 +0100

    Server validation success

--- resources/js/calculator.js
+++ resources/js/calculator.js
@@ -3,24 +3,26 @@ $(document).ready(function() {
 
   $('#calculator-form').submit(function(e){
     e.preventDefault();
-    $("#calcSubmitBtn").html("Přepočítat");
-    $("#calcSubmitBtn").css("background-color", "white");
-    $("#calcSubmitBtn").css("color", "#D40511");
     $("#orderBtn").removeClass("disabled");
     $("#phoneLink").removeClass("disabled");
     $("#emailLink").removeClass("disabled");
     $(".delimeter-container").removeClass("disabled");
+    $("#calcSubmitBtn").attr('disabled', 'disabled');
     $(".server-wait").show();
+    if ($(window).width() < 768) {
+      $('html, body').animate({
+        scrollTop: $("#result").offset().top - 70
+      }, 550, $.bez([0.22, 0.61, 0.35, 1]));
+    }
+
+    function validationSuccess() {
+      $("#calcSubmitBtn").removeAttr("disabled");
+      $("#calcSubmitBtn").html("Přepočítat");
+      $("#calcSubmitBtn").css("background-color", "white");
+      $("#calcSubmitBtn").css("color", "#D40511");
+      $("#serverCircle").addClass("green");
+    }
 
 
 
-  if ($(window).width() < 768) {
-    $('html, body').animate({
-      scrollTop: $("#result").offset().top - 70
-    }, 550, $.bez([0.22, 0.61, 0.35, 1]));
-  }
-});
-
-
-
-});
+  });
