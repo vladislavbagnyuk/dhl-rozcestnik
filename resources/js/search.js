9517ab8a88f6bd4cc68636bfe5f41e98de7f99be
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-03-08 12:41:06 +0100

    Disabled search suggestions

--- resources/js/search.js
+++ resources/js/search.js
@@ -7,22 +7,22 @@ $(document).ready(function() {
     dropdown.css('height', 'auto');
   });
 
-$('.autocomplete ul li').click(function(){
-  var text = $(this).contents().get(0).nodeValue;
-  input.val(text);
-  input.removeClass("search-focused");
-  dropdown.css('height', 0);
-  dropdown.hide();
-});
+  $('.autocomplete ul li').click(function(){
+    var text = $(this).contents().get(0).nodeValue;
+    input.val(text);
+    input.removeClass("search-focused");
+    dropdown.css('height', 0);
+    dropdown.hide();
+  });
 
-$(document).on("click", function(event){
-  if(!$(event.target).closest(".search input[type=text]").length){
-    if(!$(event.target).closest(".autocomplete").length){
-      input.removeClass("search-focused");
-      dropdown.css('height', 0);
-      dropdown.hide();
+  $(document).on("click", function(event){
+    if(!$(event.target).closest(".search input[type=text]").length){
+      if(!$(event.target).closest(".autocomplete").length){
+        input.removeClass("search-focused");
+        dropdown.css('height', 0);
+        dropdown.hide();
+      }
     }
-  }
-});
+  });
 
 });
