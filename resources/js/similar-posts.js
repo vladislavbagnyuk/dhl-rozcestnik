4650b54284743eb12dce2667d293101dfc2e4bae
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-03-11 20:51:45 +0100

    Similar posts animation

--- resources/js/similar-posts.js
+++ resources/js/similar-posts.js
@@ -1,25 +1,29 @@
+function similarPosts() {
+  if ($(window).width() >= 1024) {
+    $defaultRowWidth = 880;
+    $(".similar-posts").width($defaultRowWidth * 3 - 11);
+    $(".similar-posts").css("left", $defaultRowWidth * -1 + "px");
+    $("#similarRight").click(function(){
+      $left = parseInt($(".similar-posts").css("left")) - $defaultRowWidth;
+      if ($left >= -1760) {
+        $(".similar-posts").animate({
+          left: $left
+        }, 400, $.bez([0.22, 0.61, 0.35, 1]) );
+      }
+    });
+    $("#similarLeft").click(function(){
+      $left = parseInt($(".similar-posts").css("left")) + $defaultRowWidth;
+      if ($left <= 0) {
+        $(".similar-posts").animate({
+          left: $left
+        }, 400, $.bez([0.22, 0.61, 0.35, 1]) );
+      }
+    });
+  }
+}
 $(document).ready(function() {
-  //$defaultRowWidth = $(".similar-posts").width();
-  $defaultRowWidth = 880;
-  console.log($defaultRowWidth);
-  $(".similar-posts").width($defaultRowWidth * 3 - 11);
-  $(".similar-posts").css("left", $defaultRowWidth * -1 + "px");
-  $("#similarRight").click(function(){
-    $left = parseInt($(".similar-posts").css("left")) - $defaultRowWidth;
-    console.log($left);
-    if (left >= 1760) {
-      $(".similar-posts").animate({
-        left: $left
-      });  
-    }
-  });
-  $("#similarLeft").click(function(){
-    $left = parseInt($(".similar-posts").css("left")) + $defaultRowWidth;
-    console.log($left);
-    if ($left <= 0) {
-      $(".similar-posts").animate({
-        left: $left
-      });
-    }
-  });
+  similarPosts();
+  $(window).resize(function(){
+    similarPosts();
+  })
 });
