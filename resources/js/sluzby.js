03dc24a45bdda777df64efd8b3a65c6777898490
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-03-14 12:41:36 +0100

    Sluzby mobile js 1

--- resources/js/sluzby.js
+++ resources/js/sluzby.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $("#sluzbyMobileToggler").click(function(){
+    $hide = $(this).parent().children(".sluzby-hide");
+    if ($hide.height() != 0) {
+      $hide.animate({
+        height: 0
+      }, 300, $.bez([0.22, 0.61, 0.35, 1]) );
+    } else {
+      $hide.animate({
+        height: 0
+      }, 300, $.bez([0.22, 0.61, 0.35, 1]) );
+    }
+  });
 });
