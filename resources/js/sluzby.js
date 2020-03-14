f292afce9b668a23b44bff7ce7f510974035ebe4
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-03-14 14:19:29 +0100

    Smaller and bigger card switching

--- resources/js/sluzby.js
+++ resources/js/sluzby.js
@@ -4,11 +4,15 @@ $(document).ready(function() {
     if ($hide.height() != 0) {
       $hide.animate({
         height: 0
-      }, 300, $.bez([0.22, 0.61, 0.35, 1]) );
+      }, 400, $.bez([0.55, 0.05, 0.68, 1]) );
+      $(".last-border").css("border-bottom", "none");
+      $(this).html('Zobrazit více<img src="resources/img/icons/arrow-down-red.svg" alt="">')
     } else {
       $hide.animate({
-        height: 0
-      }, 300, $.bez([0.22, 0.61, 0.35, 1]) );
+        height: $hide.get(0).scrollHeight
+      }, 400, $.bez([0.22, 0.61, 0.35, 1]) );
+      $(".last-border").css("border-bottom", "1px solid black");
+      $(this).html('Zobrazit méně<img src="resources/img/icons/arrow-up-red.svg" alt="">');
     }
   });
 });
